2f43f4dd710fc1b392e9ac7d5effa7c87a255d02
M Sar <whaleman81@gmail.com>
2014-06-26 11:15:33 -0400

    commented out splicealign... will work on it soon

--- euk.py
+++ euk.py
@@ -4,11 +4,11 @@ out= sys.argv[1]
 query = sys.argv[2]
 SeqUtil.rename('euk-'+out+'.fas')
 os.system('prank/bin/prank -d=euk-'+out+' -o=aligns/euk-'+out+' -f=nexus -quiet')
-SeqUtil.splicealign('aligns/euk-'+out+'.best.nex','Bayes/euk-'+out+'-mod.nxs')
+#SeqUtil.splicealign('aligns/euk-'+out+'.best.nex','Bayes/euk-'+out+'-mod.nxs')
 #models=SeqUtil.bestmod('Bayes/euk-'+out+'-mod.nxs')
 models_ori=SeqUtil.bestmod('aligns/euk-'+out+'.best.nex')
-
-SeqUtil.bayesfile('Bayes/euk-'+out+'-mod.nxs',models,'Bayes/euk-'+out+'-bayes.nxs')
+SeqUtil.bayesfile('aligns/euk-'+out+'.best.nex',models_ori,'Bayes/euk-'+out+'-bayes.nxs')
+#SeqUtil.bayesfile('Bayes/euk-'+out+'-mod.nxs',models,'Bayes/euk-'+out+'-bayes.nxs')
 os.system('mb Bayes/euk-'+out+'-bayes.nxs')
 #SeqUtil.pamlseqnex('Bayes/euk-'+out+'-mod.nxs','ML/euk-'+out)
 #for mod in models.keys():
