798957505be50b02bbb10dee0ecd3e6f625cc247
solarknow <whaleman81@gmail.com>
2014-06-30 16:21:12 -0400

    Reverting to manually installing prank,phyml, and prottest

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ try:
   from setuptools import setup
 except ImportError:
   from distutils.core import setup
-from urllib import urlretrieve
-import tarfile, zipfile
+#from urllib import urlretrieve
+#import tarfile, zipfile
 
 
 setup(name='pevolution',
@@ -13,13 +13,13 @@ setup(name='pevolution',
       description='This is a pipeline to find, align, and find trees for putatively related proteins.',
       author='Mihir Sarwade',
       author_email='mihir.sarwade@effem.com',
-#      packages=['pevolution'],
+      packages=['pevolution'],
       install_requires=['psutil >= 1.2.1','biopython >= 1.64'])
 
-urlretrieve("https://prank-msa.googlecode.com/files/prank.linux64.140110.tgz",'/tmp/prank.tgz')
-urlretrieve("http://www.atgc-montpellier.fr/download/binaries/phyml/PhyML-3.1.zip",'/tmp/PhyML.zip')
-urlretrieve("http://www.clustal.org/download/current/clustalw-2.1-linux-x86_64-libcppstatic.tar.gz",'/tmp/clustalw.tar.gz')
-tarfile.open('/tmp/prank.tgz').extractall(path='/usr/local/bin/')
-zipfile.ZipFile('/tmp/PhyML.zip').extractall(path='/usr/local/bin/')
-tarfile.open('/tmp/clustalw.tar.gz').extractall(path='.')
-tarfile.open('prottest-3.4-20140123.tar.gz').extractall(path='/usr/local/bin/')
+#urlretrieve("https://prank-msa.googlecode.com/files/prank.linux64.140110.tgz",'/tmp/prank.tgz')
+#urlretrieve("http://www.atgc-montpellier.fr/download/binaries/phyml/PhyML-3.1.zip",'/tmp/PhyML.zip')
+#urlretrieve("http://www.clustal.org/download/current/clustalw-2.1-linux-x86_64-libcppstatic.tar.gz",'/tmp/clustalw.tar.gz')
+#tarfile.open('/tmp/prank.tgz').extractall(path='/usr/local/bin/')
+#zipfile.ZipFile('/tmp/PhyML.zip').extractall(path='/usr/local/bin/')
+#tarfile.open('/tmp/clustalw.tar.gz').extractall(path='.')
+#tarfile.open('prottest-3.4-20140123.tar.gz').extractall(path='/usr/local/bin/')
