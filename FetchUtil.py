f1a4694d0a832288e6f1b6a7fde7f9fac355af86
M Sar <whaleman81@gmail.com>
2018-09-21 23:30:34 -0700

    fixed FetchUtil test and started on SeqUtil

---
+++ FetchUtil.py
@@ -0,0 +1,99 @@
+from Bio import Entrez
+import subprocess,os
+
+def set_email(email):
+    Entrez.email=email
+
+def fetch_protein(acc, fmt='fasta'):
+    "returns a file-like handle of a Entrez search for acc accession number and returns in fmt format"
+    fetched = Entrez.efetch(db='protein',id=str(acc),rettype=fmt)
+    return fetched.readlines()
+
+def fetch_fasta(acc):
+    "prints the sequence in fasta format to file"
+    hand = fetch_protein(acc)
+    string=''
+    org = fetch_organism(acc)
+    string+='>'+org[0]+': '+org[1]+' '+acc+'\n'
+    for line in hand[1:]:
+        string+=line
+        if line == '\n':
+            if not os.path.exists('Orthos'):
+                subprocess.call(['mkdir','Orthos'])
+            with open('Orthos/'+acc+'.fasta','w') as writ_file:
+                writ_file.write(string)
+            return 'Orthos/'+acc+'.fasta'
+
+
+def fetch_definition(acc):
+    "Fetchs definition of specified gene product"
+    hand=fetch_protein(acc,'gp')
+    record=''
+    for line in hand:
+        read=line
+        record+=read.strip()+'  '
+    rec=record.split('  ')
+    recun=[]
+    for s in rec:
+        if s!='':
+            recun.append(s)
+    i=0
+    name=''
+    while i<len(recun):
+        if recun[i].endswith('DEFINITION'):
+            while i:
+                i+=1
+                if recun[i].startswith('ACCESSION'):
+                    return name.strip()
+                name+=recun[i]+' '
+        i+=1
+
+
+def fetch_organism(acc):
+    "Fetchs the organism from which acc accession number of a protein came from"
+    hand=fetch_protein(acc,'gp')
+    record=''
+    for line in hand:
+        read=line
+        record+=read.strip()+'  '
+    rec=record.split('  ')
+    recun=[]
+    for s in rec:
+        if s!='':
+            recun.append(s)
+    i=0
+    ret=[]
+    retu=['']
+    multisp=False
+    while i<len(recun):
+        if recun[i]=='DEFINITION':
+            multisp=recun[i+1].startswith("MULTISPECIES:")
+            if not multisp:
+              tab=recun[i+1].strip().split()
+              if not recun[i+2]=='ACCESSION':
+                  tab+=recun[i+2].strip().split()
+              for k in range(len(tab)):
+                  if tab[k].startswith('[') and len(tab[k])>5:
+                      retu=[tab[k][1:]]
+                      while k<len(tab):
+                          k+=1
+                          if tab[k].endswith('].'):
+                              retu[0]+=' '+tab[k][0:tab[k].index(']')]
+                              retu.append('Unknown.')
+                              break
+                          else:
+                              retu[0]+=' '+tab[k]
+            else:
+              return [recun[i+11]+' multispecies',recun[i+14].split(';')[0]]
+
+        if recun[i]=='ORGANISM':
+            try:
+                ret=[recun[i+1].split()[0]+' '+recun[i+1].split()[1]]
+            except:
+                return retu
+            if ret[0].endswith('.'):
+                ret[0]+=' '+recun[i+1].split()[2]
+            ret.append(recun[i+2].split(';')[0])
+            return ret
+
+        i+=1
---
+++ test/test_SeqUtil.py
@@ -0,0 +1,38 @@
+import unittest
+import SeqUtil
+from Bio import Entrez
+
+class TestSeqUtil(unittest.TestCase):
+    def setUp(self):
+        Entrez.email = 'example@gmail.com'
+        self.test_accession = 'AJA33470.1'
+        self.expected_organism = ['Vibrio sp. AN61', 'Bacteria']
+        self.expected_path = 'Orthos/AJA33470.1.fasta'
+        self.expected_definition = 'MreB, partial [Vibrio sp. AN61].'
+        
+    def test_findlonglen_returns_longest_length(self):
+        test_dict = {
+            'length1': 15,
+            'length10': 150,
+            'length100': 1,
+        }
+        self.assertEqual(len('length100'), SeqUtil.findlonglen(test_dict))
+    
+    def test_fetch_protein_returns_list(self):
+        test_proteins = Fetchutil.fetch_protein(self.test_accession)
+        self.assertIsInstance(test_proteins, list)
+    
+    def test_fetch_organism_returns_expected_list(self):
+        test_organism = Fetchutil.fetch_organism(self.test_accession)
+        self.assertAlmostEqual(test_organism, self.expected_organism)
+        
+    def test_fetch_fasta_returns_path(self):
+        test_path = Fetchutil.fetch_fasta(self.test_accession)
+        self.assertEqual(test_path, self.expected_path)
+    
+    def test_fetch_definition_returns_definition(self):
+        test_definition = Fetchutil.fetch_definition(self.test_accession)
+        self.assertEqual(test_definition, self.expected_definition)
+    
+if __name__ == '__main__':
+    unittest.main()
