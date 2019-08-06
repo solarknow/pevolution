b9f7d710bd76b33953e10e39fbb7dce0220bc5f7
M Sar <whaleman81@gmail.com>
2019-08-05 21:05:06 -0700

    first round on fetchutil

--- FetchUtil.py
+++ FetchUtil.py
@@ -1,5 +1,4 @@
 import os
-import subprocess
 
 from Bio import Entrez
 
@@ -19,86 +18,70 @@ def fetch_fasta(acc):
     hand = fetch_protein(acc)
     string = ''
     org = fetch_organism(acc)
-    string += '>'+org[0]+': '+org[1]+' '+acc+'\n'
+    string += '>' + org[0] + ': ' + org[1] + ' ' + acc + '\n'
     for line in hand[1:]:
         string += line
         if line == '\n':
             if not os.path.exists('Orthos'):
-                subprocess.call(['mkdir', 'Orthos'])
-            with open('Orthos/'+acc+'.fasta','w') as writ_file:
+                os.mkdir('Orthos')
+            with open('Orthos/' + acc + '.fasta', 'w') as writ_file:
                 writ_file.write(string)
-            return 'Orthos/'+acc+'.fasta'
+            return 'Orthos/' + acc + '.fasta'
 
 
 def fetch_definition(acc):
-    """Fetchs definition of specified gene product"""
-    hand = fetch_protein(acc,'gp')
-    record = ''
-    for line in hand:
-        read = line
-        record+=read.strip()+'  '
-    rec=record.split('  ')
-    recun=[]
-    for s in rec:
-        if s!='':
-            recun.append(s)
-    i=0
-    name=''
-    while i<len(recun):
-        if recun[i].endswith('DEFINITION'):
+    """Fetches definition of specified gene product"""
+    hand = fetch_protein(acc, 'gp')
+    records = '  '.join([line.strip() for line in hand])
+    filtered_records = [record for record in records.split('  ') if record]
+    i = 0
+    name = ''
+    while i < len(filtered_records):
+        if filtered_records[i].endswith('DEFINITION'):
             while i:
-                i+=1
-                if recun[i].startswith('ACCESSION'):
+                i += 1
+                if filtered_records[i].startswith('ACCESSION'):
                     return name.strip()
-                name+=recun[i]+' '
-        i+=1
+                name += filtered_records[i] + ' '
+        i += 1
 
 
 def fetch_organism(acc):
-    "Fetchs the organism from which acc accession number of a protein came from"
-    hand=fetch_protein(acc,'gp')
-    record=''
-    for line in hand:
-        read=line
-        record+=read.strip()+'  '
-    rec=record.split('  ')
-    recun=[]
-    for s in rec:
-        if s!='':
-            recun.append(s)
-    i=0
-    ret=[]
-    retu=['']
-    multisp=False
-    while i<len(recun):
-        if recun[i]=='DEFINITION':
-            multisp=recun[i+1].startswith("MULTISPECIES:")
-            if not multisp:
-              tab=recun[i+1].strip().split()
-              if not recun[i+2]=='ACCESSION':
-                  tab+=recun[i+2].strip().split()
-              for k in range(len(tab)):
-                  if tab[k].startswith('[') and len(tab[k])>5:
-                      retu=[tab[k][1:]]
-                      while k<len(tab):
-                          k+=1
-                          if tab[k].endswith('].'):
-                              retu[0]+=' '+tab[k][0:tab[k].index(']')]
-                              retu.append('Unknown.')
-                              break
-                          else:
-                              retu[0]+=' '+tab[k]
+    """Fetches the organism from which acc accession number of a protein came from"""
+    hand = fetch_protein(acc, 'gp')
+    records = '  '.join([line.strip() for line in hand])
+    filtered_records = [record for record in records.split('  ') if record]
+    i = 0
+    retu = ['']
+    while i < len(filtered_records):
+        if filtered_records[i] == 'DEFINITION':
+            multispecies = filtered_records[i + 1].startswith("MULTISPECIES:")
+            if not multispecies:
+                tab = filtered_records[i + 1].strip().split()
+                if not filtered_records[i + 2] == 'ACCESSION':
+                    tab += filtered_records[i + 2].strip().split()
+                for k in range(len(tab)):
+                    if tab[k].startswith('[') and len(tab[k]) > 5:
+                        retu = [tab[k][1:]]
+                        while k < len(tab):
+                            k += 1
+                            if tab[k].endswith('].'):
+                                retu[0] += ' ' + tab[k][0:tab[k].index(']')]
+                                retu.append('Unknown.')
+                                break
+                            else:
+                                retu[0] += ' ' + tab[k]
             else:
-              return [recun[i+11]+' multispecies',recun[i+14].split(';')[0]]
+                return [filtered_records[i + 11] + ' multispecies', filtered_records[i + 14].split(';')[0]]
 
-        if recun[i]=='ORGANISM':
+        if filtered_records[i] == 'ORGANISM':
             try:
-                ret=[recun[i+1].split()[0]+' '+recun[i+1].split()[1]]
-            except:
+                ret = [filtered_records[i + 1].split()[0] + ' ' + filtered_records[i + 1].split()[1]]
+            except IndexError:
                 return retu
             if ret[0].endswith('.'):
-                ret[0]+=' '+recun[i+1].split()[2]
-            ret.append(recun[i+2].split(';')[0])
+                ret[0] += ' ' + filtered_records[i + 1].split()[2]
+            ret.append(filtered_records[i + 2].split(';')[0])
             return ret
 
-        i+=1
+        i += 1
